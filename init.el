613952af76992749fea9567fd0f866bbc6b868fb
Johannes Becker <alfalfasprossen@gmail.com>
2016-02-03 15:53:31 +0100

    Only load mac and windows-settings when on mac/win
    Only execute the load of the specific settings file when on the
    respective system to avoid having to capsulate the whole content of
    these files into such a statement.

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-;(load "init_windows") ; windows specific 
+;(load "init_windows") ; windows specific
 
-;; make sure package installed packages are found before I use them 
+;; make sure package installed packages are found before I use them
 ;; inside my own init files
 (setq package-enable-at-startup nil)
 (load "init_elpa.el")
@@ -11,10 +11,12 @@
 ;; initialize my keyboard settings before trying something stupid
 (load "init_keyboard") ; so if something fails, i still have MY KEYBOARD SETTINGS!!!
 
-(load "init_mac") ; mac specific
-(load "init_settings") ; lots of general settings 
+(when (eq system-type 'darwin) ;; mac specific settings
+  (load "init_mac")) ; mac specific
+(load "init_settings") ; lots of general settings
 (load "init_window") ; visual appearance
-(load "init_windows")
+(when (eq system-type 'windows-nt) ;; windows specific
+  (load "init_windows"))
 (load "init_progmodes") ;
 
 
@@ -139,11 +141,11 @@
 
  (if (not filename)
 	(message "Buffer '%s' is not visiting a file!" name)
- (progn 	(copy-file filename newname 1) 	(delete-file filename) 	(set-visited-file-name newname) 	(set-buffer-modified-p nil) 	t)))) 
+ (progn 	(copy-file filename newname 1) 	(delete-file filename) 	(set-visited-file-name newname) 	(set-buffer-modified-p nil) 	t))))
 
 ;;; python intelly sense stuff
 ;; pymacs
-;(add-to-list 'load-path "~/.emacs.d/lisp/pymacs") 
+;(add-to-list 'load-path "~/.emacs.d/lisp/pymacs")
 ;(autoload 'pymacs-apply "pymacs")
 ;(autoload 'pymacs-call "pymacs")
 ;(autoload 'pymacs-eval "pymacs" nil t)
@@ -152,29 +154,29 @@
 ;(autoload 'pymacs-autoload "pymacs")
 ;;;(eval-after-load "pymacs"
 ;;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
-; 
+;
 ;; flymake for python with pyflakes
-;(when (load "flymake" t) 
-;     (defun flymake-pyflakes-init () 
-;       (let* ((temp-file (flymake-init-create-temp-buffer-copy 
-;                          'flymake-create-temp-inplace)) 
-;      (local-file (file-relative-name 
-;               temp-file 
-;               (file-name-directory buffer-file-name)))) 
+;(when (load "flymake" t)
+;     (defun flymake-pyflakes-init ()
+;       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;                          'flymake-create-temp-inplace))
+;      (local-file (file-relative-name
+;               temp-file
+;               (file-name-directory buffer-file-name))))
 ;         (list "pyflakes" (list local-file))))
-; 
-;     (add-to-list 'flymake-allowed-file-name-masks 
+;
+;     (add-to-list 'flymake-allowed-file-name-masks
 ;          '("\\.py\\'" flymake-pyflakes-init)))
-; 
+;
 ;(add-hook 'find-file-hook 'flymake-find-file-hook)
-; 
+;
 ;(defun my-flymake-show-help ()
 ;  (when (get-char-property (point) 'flymake-overlay)
 ;   (let ((help (get-char-property (point) 'help-echo)))
 ;    (if help (message "%s" help)))))
-; 
+;
 ;(add-hook 'post-command-hook 'my-flymake-show-help)
-; 
+;
 ;; auto complete for rope
 ;(ac-ropemacs-initialize)
 ;(add-hook 'python-mode-hook
@@ -190,7 +192,7 @@
 
 
 ; -
-; - 
+; -
 ;;----------------------------------------------------------------------------
 
 
@@ -201,7 +203,6 @@
 (load "init_keyboard") ; and now make sure no keys have been overriden by some packages
 (ido-mode 1)
 (setq ido-enable-flex-matching 1)
-(company-quickhelp-mode 0)
 
 
 
