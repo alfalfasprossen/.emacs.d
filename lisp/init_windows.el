fe8fc071b2cfa6a7249ee70306ddb2431e051d8f
Johannes Becker <alfalfasprossen@gmail.com>
2016-07-29 09:49:19 +0200

    Fix shell mode in emacs on windows

--- lisp/init_windows.el
+++ lisp/init_windows.el
@@ -9,3 +9,20 @@
   (server-start))
 
 (set-default-font "Lucida Console 12")
+
+(setq explicit-shell-file-name
+      "C:/Program Files/Git/bin/sh.exe")
+(setq shell-file-name explicit-shell-file-name)
+(add-to-list 'exec-path "C:/Program Files/Git/bin/")
+
+
+(defun my-shell-setup ()
+  "For Cygwin bash under Emacs 20"
+  (setq comint-scroll-show-maximum-output 'this)
+  (make-variable-buffer-local 'comint-completion-addsuffix))
+  (setq comint-completion-addsuffix t)
+  ;; (setq comint-process-echoes t) ;; reported that this is no longer needed
+  (setq comint-eol-on-send t)
+  (setq w32-quote-process-args ?\")
+
+(add-hook 'shell-mode-hook 'my-shell-setup)
