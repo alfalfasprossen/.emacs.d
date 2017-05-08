63b150b0f0ce7a079de1f362ea8a400461acbbbf
Johannes Becker <alfalfasprossen@gmail.com>
2017-05-08 09:39:37 +0200

    Add fix for company popups being displaced by fci-mode

--- init.el
+++ init.el
@@ -34,6 +34,22 @@
 ;; -
 ;; -
 
+;; Fix for company popups being displaced by fci-mode
+(defvar-local company-fci-mode-on-p nil)
+
+(defun company-turn-off-fci (&rest ignore)
+  (when (boundp 'fci-mode)
+    (setq company-fci-mode-on-p fci-mode)
+    (when fci-mode (fci-mode -1))))
+
+(defun company-maybe-turn-on-fci (&rest ignore)
+  (when company-fci-mode-on-p (fci-mode 1)))
+
+(add-hook 'company-completion-started-hook 'company-turn-off-fci)
+(add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+
+
 (defun --copy-face (new-face face)
   "Define NEW-FACE from existing FACE."
   (copy-face face new-face)
